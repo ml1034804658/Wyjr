943940fad05af3b729362996d7ca0f5c6559cec3
ml1034804658 <1034804658@qq.com>
2019-06-24 21:51:42 +0800

    Initial commit

---
+++ src/test/java/com/jr/wyjr/WyjrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jr.wyjr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WyjrApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
