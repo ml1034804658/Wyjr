e0af772c3aa732e8701cd4497858e12e0e21fa75
ml1034804658 <1034804658@qq.com>
2019-06-27 20:48:23 +0800

    tijiao

--- Wyjr_Entity/src/main/java/com/jr/entity/Tb_Question.java
+++ Wyjr_Entity/src/main/java/com/jr/entity/Tb_Question.java
@@ -7,17 +7,17 @@ import java.util.Date;
 public class Tb_Question {
     private Long qid;
 
-    private Long uid;
+    private Long uid;         //用户id
 
-    private String content;
+    private String content;    //内容
 
-    private Date cretetime;
+    private Date cretetime;      //创建时间
 
-    private String imageurl;
+    private String imageurl;     //图片路径
 
-    private Integer state;
+    private Integer state;      //采纳状态  1.被采纳 0.没被采纳
 
-    private Integer reward;
+    private Integer reward;     //赏金
 
 
 }
--- Wyjr_Entity/src/main/java/com/jr/entity/Tb_Question_Answer.java
+++ Wyjr_Entity/src/main/java/com/jr/entity/Tb_Question_Answer.java
@@ -7,17 +7,17 @@ import java.util.Date;
 public class Tb_Question_Answer {
     private Long id;
 
-    private Long qid;
+    private Long qid;             //问答id
 
-    private Long uid;
+    private Long uid;           //用户id
 
-    private Long parentId;
+    private Long parentId;       //一级id
 
-    private Long rid;
+    private Long rid;            //回复的id
 
-    private String rContent;
+    private String rContent;    //回复内容
 
-    private Date rTime;
+    private Date rTime;          //回复时间
 
 
 }
