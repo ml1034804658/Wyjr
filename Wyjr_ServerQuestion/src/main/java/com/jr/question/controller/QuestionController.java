d207c2b87caeae264e1aebea6dae93a80886ab7c
ml1034804658 <1034804658@qq.com>
2019-06-27 21:00:41 +0800

    tijiao

---
+++ Wyjr_ServerQuestion/src/main/java/com/jr/question/controller/QuestionController.java
@@ -0,0 +1,22 @@
+package com.jr.question.controller;
+
+import com.jr.entity.Tb_Question;
+import com.jr.question.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class QuestionController {
+
+    @Autowired
+    private QuestionService questionService;
+
+    @GetMapping("question.do")
+    public List<Tb_Question> findAllQuestion(){
+        return questionService.findAllquestion();
+    }
+
+}
