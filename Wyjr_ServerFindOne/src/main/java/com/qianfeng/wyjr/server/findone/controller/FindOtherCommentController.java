4ab8923f2aa863a81798b9d65fc9f38a7644a9e0
zhang <zhangyi1661@outlook.com>
2019-06-27 20:37:29 +0800

    1.增加了捡人api和service 2.在common中加入了一些工具类 3.捡人server增加类

---
+++ Wyjr_ServerFindOne/src/main/java/com/qianfeng/wyjr/server/findone/controller/FindOtherCommentController.java
@@ -0,0 +1,4 @@
+package com.qianfeng.wyjr.server.findone.controller;
+
+public class FindOtherCommentController {
+}
---
+++ Wyjr_ServerFindOne/src/main/java/com/qianfeng/wyjr/server/findone/service/impl/FindOtherServiceImpl.java
@@ -0,0 +1,6 @@
+package com.qianfeng.wyjr.server.findone.service.impl;
+
+import com.qianfeng.wyjr.server.findone.service.FindOtherService;
+
+public class FindOtherServiceImpl implements FindOtherService {
+}
